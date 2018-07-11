bd41144297208d38a6b0b2a029caa5a0e1a4d45b
Dami <obayanjud@gmail.com>
2018-07-11 16:50:14 -0600

    feat: Hide logo

--- js/designs.js
+++ js/designs.js
@@ -1,6 +1,13 @@
 function showLogo() {
-	const logo = document.querySelector(".slack");
-	// remove black background
-	logo.parentElement.style.backgroundColor = 'white';
-	logo.style.visibility = "visible";
+  const logo = document.querySelector(".slack");
+  // remove black background
+  logo.parentElement.style.backgroundColor = "white";
+  logo.style.visibility = "visible";
+}
+
+function hideLogo() {
+  const logo = document.querySelector(".slack");
+  // remove black background
+  logo.parentElement.style.backgroundColor = "black";
+	logo.style.visibility = "hidden";
 }
