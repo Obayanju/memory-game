0d91af498b0fc130dc9588ba5170bd938376b229
Dami <obayanjud@gmail.com>
2018-07-11 18:27:58 -0600

    fix: show logo fix to work with every cell's logo
    previously, it could only show the first cell's logo

--- js/designs.js
+++ js/designs.js
@@ -6,22 +6,21 @@ function hide_or_showLogo(event) {
   const card = event.target;
   // make sure we are targetting the card div or the image and not the parent wrapper div
   if (card.className == "slack" || card.className == "card") {
-		// the div's have their visibility set to "" by default,
-		// we haven't set their visibility explicitly like the images which are explicitly hidden
+    // the div's have their visibility set to "" by default,
+    // we haven't set their visibility explicitly like the images which are explicitly hidden
     if (card.style.visibility == "" || card.style.visibility == "hidden") {
-      showLogo();
+      showLogo(event.target);
     } else {
-      hideLogo();
+      hideLogo(event.target);
     }
   }
 }
 
 // console.log(event.target.style.visibility);
-function showLogo() {
-  const logo = document.querySelector(".slack");
-  // remove black background
-  logo.parentElement.style.backgroundColor = "white";
-  logo.style.visibility = "visible";
+function showLogo(card) {
+  // NB: only the card div can be clicked since the image is hidden
+  card.style.backgroundColor = "white"; // show the logo
+  card.firstElementChild.style.visibility = "visible";
 }
 
 function hideLogo() {
