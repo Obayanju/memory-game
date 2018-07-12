ccf4858b1feb158f577ed9bc6f9cbaa6b83c36ac
Dami <obayanjud@gmail.com>
2018-07-11 18:19:48 -0600

    feat: Hide or show logo
    second logo was also added for testing purposes(should be a separate commit, but got lost finding when I wrote it)

--- js/designs.js
+++ js/designs.js
@@ -1,3 +1,22 @@
+// select the whole grid
+const grid_wrapper = document.querySelector(".wrapper");
+grid_wrapper.addEventListener("click", hide_or_showLogo);
+
+function hide_or_showLogo(event) {
+  const card = event.target;
+  // make sure we are targetting the card div or the image and not the parent wrapper div
+  if (card.className == "slack" || card.className == "card") {
+		// the div's have their visibility set to "" by default,
+		// we haven't set their visibility explicitly like the images which are explicitly hidden
+    if (card.style.visibility == "" || card.style.visibility == "hidden") {
+      showLogo();
+    } else {
+      hideLogo();
+    }
+  }
+}
+
+// console.log(event.target.style.visibility);
 function showLogo() {
   const logo = document.querySelector(".slack");
   // remove black background
@@ -9,5 +28,5 @@ function hideLogo() {
   const logo = document.querySelector(".slack");
   // remove black background
   logo.parentElement.style.backgroundColor = "black";
-	logo.style.visibility = "hidden";
+  logo.style.visibility = "hidden";
 }
